04782da9a0b8d78bd16ad90ea351aee2c65fc131
Pierre Iraguha <petrus@Pierres-MacBook-Pro.local>
2018-02-20 11:43:56 -0500

    Initial commit

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  template: `
+    <div class="row">
+      <div class="col-md-5">
+        <app-recipe-list></app-recipe-list>
+      </div>
+      <div class="col-md-7">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+    `,
+   styles: []
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
